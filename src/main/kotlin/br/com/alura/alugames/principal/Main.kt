acf028c93e2023c97cf8647f6cd758e04057fe0d
Guilherme Mergulhão <golimpiof@gmail.com>
2025-05-15 13:43:46 -0300

    manipulando listas e trabalhando com extension functions

--- src/main/kotlin/br/com/alura/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/Main.kt
@@ -1,65 +1,92 @@
 package org.example.br.com.alura.alugames.principal
 
+import br.com.alura.alugames.modelo.Gamer
 import br.com.alura.alugames.servicos.ConsumoApi
 import org.example.br.com.alura.alugames.modelo.Jogo
+import transformarEmIdade
 import java.util.Scanner
 
 
 fun main() {
     val leitura = Scanner(System.`in`)
-    println("Digite código de jogo para buscar:")
-    val busca = leitura.nextLine()
+    val gamer = Gamer.criarGamer(leitura)
+    println("Cadastro concluído com sucesso. Dados do gamer:")
+    println(gamer)
+    println("Idade do gamer: " + gamer.dataNascimento?.transformarEmIdade())
 
-    val buscaApi = ConsumoApi()
-    val informacaoJogo = buscaApi.buscaJogo(busca)
+    do {
+        println("Digite código de jogo para buscar:")
+        val busca = leitura.nextLine()
 
-    var meuJogo: Jogo? = null
+        val buscaApi = ConsumoApi()
+        val informacaoJogo = buscaApi.buscaJogo(busca)
 
-    if (informacaoJogo != null) {
-        meuJogo = Jogo(informacaoJogo.info.title, informacaoJogo.info.thumb)
-    } else {
-        return
+        var meuJogo: Jogo? = null
+
+        if (informacaoJogo != null) {
+            meuJogo = Jogo(informacaoJogo.info.title, informacaoJogo.info.thumb)
+        } else {
+            return
+        }
+
+        val resultado = runCatching {
+            meuJogo = Jogo(informacaoJogo.info.title, informacaoJogo.info.thumb)
+        }
+
+        resultado.onFailure {
+            println("Jogo inexistente. Tente outro id")
+        }
+
+        resultado.onSuccess {
+            println("Você quer inserir uma descrição para esse jogo? (S/N)")
+            val opcao = leitura.nextLine()
+            if (opcao.equals("S", true)) {
+
+                println("Insira a descrição abaixo:\n")
+                meuJogo?.descricao = leitura.nextLine()
+
+            } else {
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+
+            gamer.jogosBuscados.add(meuJogo)
+        }
+
+        println("Deseja buscar um novo jogo? S/N")
+        val resposta = leitura.nextLine()
+
+    } while (resposta.equals("s",true))
+
+    println("Jogos buscados:")
+    println(gamer.jogosBuscados)
+
+    println("\nJogos ordenados por título: ")
+    gamer.jogosBuscados.sortBy {
+        it?.titulo
     }
 
-//    val resultado = runCatching {
-//
-//        if (informacaoJogo != null) {
-//            meuJogo = Jogo(informacaoJogo.info.title, informacaoJogo.info.thumb)
-//        } else {
-//            return
-//        }
-//    }
-    println("Você quer inserir uma descrição para esse jogo? (S/N)")
+    gamer.jogosBuscados.forEach {
+        println("Título: " + it?.titulo)
+    }
+
+    val jogosFiltrados = gamer.jogosBuscados.filter {
+        it?.titulo?.contains("batman", true) ?: false
+    }
+    println("\nJogos filtrados: ")
+    println(jogosFiltrados)
+
+    println("\nDeseja excluir algum jogo da lista original? S/N")
     val opcao = leitura.nextLine()
-    if (opcao.equals("S", true)) {
-
-        println("Insira a descrição abaixo:\n")
-        meuJogo?.descricao = leitura.nextLine()
-
-    } else {
-        meuJogo?.descricao = meuJogo?.titulo
+    if (opcao.equals("s", true)) {
+        println(gamer.jogosBuscados)
+        println("\nInforme a posição do jogo que deseja excluir: ")
+        val posicao = leitura.nextInt()
+        gamer.jogosBuscados.removeAt(posicao - 1)
     }
 
-    println(meuJogo)
-    println("Busca realizada com sucesso!")
+    println("\nLista atualizada: ")
+    println(gamer.jogosBuscados)
 
-//    resultado.onSuccess {
-//        println("Você quer inserir uma descrição para esse jogo? (S/N)")
-//        val opcao = leitura.nextLine()
-//        if (opcao.equals("S", true)) {
-//
-//            println("Insira a descrição abaixo:\n")
-//            meuJogo?.descricao = leitura.nextLine()
-//
-//        } else {
-//            meuJogo?.descricao = meuJogo?.titulo
-//        }
-//
-//        println(meuJogo)
-//    }
-//
-//    resultado.onSuccess {
-//        println("Busca realizada com sucesso!")
-//    }
+    println("Busca realizada com sucesso!")
 
 }
